325eab83d1e156ad61d7ba0860972927a9cbec78
BlackHat-17 <adityawaghmare1705@gmail.com>
2025-04-24 03:01:55 +0530

    version 3.0

--- login_signup_ui/android/app/build.gradle.kts
+++ login_signup_ui/android/app/build.gradle.kts
@@ -7,7 +7,9 @@ plugins {
 android {
     namespace = "com.TechNudge.EasyReport"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
+
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
